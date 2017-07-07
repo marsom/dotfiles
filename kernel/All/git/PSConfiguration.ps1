21638bf0683f785fefad3dc0d65af8c57ed0486d
Markus Sommer <marsom@skooby.ch>
2017-07-07 13:41:06 +0200

    powershell updates.

--- kernel/All/git/PSConfiguration.ps1
+++ kernel/All/git/PSConfiguration.ps1
@@ -1,23 +1,34 @@
 #!/usr/bin/env powershell
 
-# gpull, gl seams to be a reserved alias :-(
-function Invoke-gpull { git pull --prune $args }
-Set-Alias gpull Invoke-gpull
+function Invoke-gl { git pull --prune $args }
+Set-Alias -Name gl Invoke-gl -Force -Option AllScope
 
-# glog
 function Invoke-glog { git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative }
 Set-Alias glog Invoke-glog
 
-#Set-Alias glog "git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-#Set-Alias gp 'git push origin HEAD'
-#Set-Alias gd 'git diff'
-#Set-Alias gc 'git commit'
-#Set-Alias gca 'git commit -a'
-#Set-Alias gco 'git checkout'
-#Set-Alias gcb 'git copy-branch-name'
-#Set-Alias gb 'git branch'
+function Invoke-gp { git push origin HEAD }
+Set-Alias -Name gp -Value Invoke-gp -Force -Option AllScope
+
+function Invoke-gd { git diff }
+Set-Alias gd Invoke-gd
+
+function Invoke-gc { git commit }
+Set-Alias -Name gc -Value Invoke-gc -Force -Option AllScope
+
+function Invoke-gca { git commit -a $args}
+Set-Alias gca Invoke-gca
+
+function Invoke-gco { git checkout $args }
+Set-Alias gca Invoke-gco
+
+function Invoke-gcb { git copy-branch-name $args }
+Set-Alias gca Invoke-gcb
+
+function Invoke-gb { git branch $args }
+Set-Alias gb Invoke-gb
 
 function Invoke-gs { git status -sb $args }
 Set-Alias gs Invoke-gs
 
-#Set-Alias gac 'git add -A && git commit -m'
+function Invoke-gac { git add -A; git commit -m }
+Set-Alias gca Invoke-gac
--- kernel/Windows/powershell/dotfiles-configure.ps1
+++ kernel/Windows/powershell/dotfiles-configure.ps1
@@ -3,3 +3,4 @@
 Import-Module  "~/.dotfiles/bin/dotfiles.psm1" -ErrorAction Stop
 
 Set-DotfilesModuleLink -src "powershell/PSConfiguration.symlink.ps1" -dst ~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+Set-DotfilesModuleLink -src "powershell/PSConfiguration.symlink.ps1" -dst ~/Documents/WindowsPowerShell/Microsoft.VSCode_profile.ps1
