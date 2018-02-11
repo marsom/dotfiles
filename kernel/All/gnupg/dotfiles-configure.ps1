fffde8d08e6c83bb74d19ae00fd5ed744be3ef5a
Markus Sommer <marsom@skooby.ch>
2018-02-11 20:59:58 +0100

    updated gnupg config files.

--- kernel/All/gnupg/dotfiles-configure.ps1
+++ kernel/All/gnupg/dotfiles-configure.ps1
@@ -3,4 +3,6 @@
 Import-Module  "~/.dotfiles/bin/dotfiles.psm1" -ErrorAction Stop
 
 New-Item -Path "~/.gnupg" -ItemType Directory -ErrorAction SilentlyContinue
+
 Set-DotfilesModuleLink -src "gnupg/gpg.conf" -dst "~/.gnupg/gpg.conf"
+Set-DotfilesModuleLink -src "gnupg/gpg-agent.conf" -dst "~/.gnupg/gpg-agent.conf"
--- kernel/All/gnupg/dotfiles-configure.sh
+++ kernel/All/gnupg/dotfiles-configure.sh
@@ -3,3 +3,4 @@
 [ -r ~/.dotfiles/bin/dotfiles ] && source  ~/.dotfiles/bin/dotfiles || exit 5
 
 dotfiles_link_module_file gnupg/gpg.conf ~/.gnupg/gpg.conf
+dotfiles_link_module_file gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
