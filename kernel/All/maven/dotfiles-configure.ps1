60f24091964a4f0168ff1dbf022de19930e41507
Markus Sommer <marsom@skooby.ch>
2018-07-26 10:09:31 +0200

    added maven toolchain.xml

--- kernel/All/maven/dotfiles-configure.ps1
+++ kernel/All/maven/dotfiles-configure.ps1
@@ -3,3 +3,4 @@
 Import-Module  "~/.dotfiles/bin/dotfiles.psm1" -ErrorAction Stop
 
 Set-DotfilesModuleLink -src "maven/settings.xml" -dst ~/.m2/settings.xml
+Set-DotfilesModuleLink -src "maven/toolchains.xml" -dst ~/.m2/toolchains.xml
--- kernel/All/maven/dotfiles-configure.sh
+++ kernel/All/maven/dotfiles-configure.sh
@@ -3,3 +3,5 @@
 [ -r ~/.dotfiles/bin/dotfiles ] && source  ~/.dotfiles/bin/dotfiles || exit 5
 
 dotfiles_link_file $(dirname $0)/mvn-cleanup-repository.sh ~/bin/mvn-cleanup-repository
+dotfiles_link_file $(dirname $0)/settings.xml ~/.m2/settings.xml
+dotfiles_link_file $(dirname $0)/toolchains.xml ~/.m2/toolchains.xml
