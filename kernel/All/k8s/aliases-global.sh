c2ac4fcb450ef1477b8271b705cd27a992564722
Markus Sommer <marsom@skooby.ch>
2018-11-21 12:12:05 +0000

    added new k8s aliass, added sslyse alias.

--- kernel/All/k8s/aliases-global.sh
+++ kernel/All/k8s/aliases-global.sh
@@ -1,2 +1,3 @@
 alias k='kubectl'
 alias ks='kubectl sudo'
+alias kevents="kubectl get events --sort-by='.metadata.creationTimestamp'"
---
+++ kernel/All/shell/aliases-global.sh
@@ -0,0 +1,1 @@
+alias 'sslyze'='docker run --rm -it nablac0d3/sslyze'
