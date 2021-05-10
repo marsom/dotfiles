2fe5392982145b0d5fcc5375112cf24cbc6e8231
Markus Sommer <marsom@skooby.ch>
2021-05-10 10:34:11 +0200

    added auto completion for kubectl

--- kernel/All/k8s/aliases-global.sh
+++ kernel/All/k8s/aliases-global.sh
@@ -1,3 +1,6 @@
 alias k='kubectl'
 alias ks='kubectl sudo'
 alias kevents="kubectl get events --sort-by='.metadata.creationTimestamp'"
+
+source <(kubectl completion bash) 
+complete -F __start_kubectl k
