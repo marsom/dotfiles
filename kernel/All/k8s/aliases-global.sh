5180f43d83ee70fd9e9a20d218e1d44136b4ad88
Markus Sommer <marsom@skooby.ch>
2021-05-12 08:52:30 +0200

    set k8s alias on system with kubectl only

--- kernel/All/k8s/aliases-global.sh
+++ kernel/All/k8s/aliases-global.sh
@@ -1,6 +1,9 @@
-alias k='kubectl'
-alias ks='kubectl sudo'
-alias kevents="kubectl get events --sort-by='.metadata.creationTimestamp'"
+if type kubectl > /dev/null 2>/dev/null; then
+  alias k='kubectl'
+  alias ks='kubectl sudo'
+  alias kevents="kubectl get events --sort-by='.metadata.creationTimestamp'"
 
-source <(kubectl completion bash) 
-complete -F __start_kubectl k
+
+  source <(kubectl completion bash) 
+  complete -F __start_kubectl k
+fi
